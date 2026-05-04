96bd67c707f0dccefec0e586bc89aaf1ffd74518
2532614 <2532614@etu.cegepjonquiere.ca>
2026-05-04 08:17:12 -0400

    ok

--- vaisseau.py
+++ vaisseau.py
@@ -2,6 +2,11 @@ class Vaisseau:
     def __init__(self, nom: str, modele: str, prix: int, cargo: int, vitesse: float, capacite_equipe: int):
         self.nom = nom
         self.modele = modele
+        self._prix = 0
+        self._cargo = 0
+        self._vitesse = 0
+        self._capacite_equipe = 0
+
         self.prix = prix
         self.cargo = cargo
         self.vitesse = vitesse
@@ -9,6 +14,11 @@ class Vaisseau:
 
 
     def to_dick(self)-> dict:
+        """transforme l'objet en dictionnaire
+
+        Returns:
+            dict: un dictionnaire
+        """
         return {"nom": self.nom, "modele": self.modele, "prix": self.prix, "cargo": self.cargo, "vitesse": self.vitesse, "capacite_equipe": self.capacite_equipe}
     
 
@@ -18,8 +28,39 @@ class Vaisseau:
         return f"{self.prix}" + " " * espaces + f"credits,   {self.nom}"
 
 
+    @property
+    def prix(self) -> int:
+        return self._prix
+    
+    @prix.setter
+    def prix(self, prix:int) -> None:
+        if prix > 0:
+            self._prix = prix
+
+    @property
+    def cargo(self) -> int:
+        return self._cargo
+    
+    @cargo.setter
+    def cargo(self, cargo:int) -> None:
+        if cargo > 0:
+            self._cargo = cargo
 
 
+    @property
+    def vitesse(self) -> int:
+        return self._vitesse
+    
+    @vitesse.setter
+    def vitesse(self, vitesse:int) -> None:
+        if vitesse > 0:
+            self._vitesse = vitesse
 
-
-        
+    @property
+    def capacite_equipe(self) -> int:
+        return self._capacite_equipe
+    
+    @capacite_equipe.setter
+    def capacite_equipe(self, capacite_equipe:int) -> None:
+        if capacite_equipe > 0:
+            self._capacite_equipe = capacite_equipe
